7525e9aefb300ee4d0f6cc331bb8e59d58087680
josefo0330 <jose_6128@hotmail.com>
2024-05-24 13:41:18 -0500

    Actualizacion 3

--- src/router/baseDataConect/conexion.js
+++ src/router/baseDataConect/conexion.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conexion = mysql.createConnection({
-    host: 'localhost',
-    database:"cruv",//'if0_36456925_cruv',
-    user:'root',
-    password:''
+    host: 'btiaphkz1zqolfvrqhg4-mysql.services.clever-cloud.com',
+    database:"btiaphkz1zqolfvrqhg4",//'if0_36456925_cruv',
+    user:'uxf3zc6yzkruyzpu',
+    password:'UwQLSRFiJORf24CmY3Mx'
 });
 conexion.connect(function(error){
     if(error)

49101813a0d439b298304bd68674559ed91e8b7a
josefo0330 <jose_6128@hotmail.com>
2025-11-24 21:16:21 -0500

    NODE JS

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ var conexion;
 //configuraciones
 app.use(morgan('dev'));
 //app.set('port',3000);
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 80);
 
 app.set('views',path.join(__dirname,'view'));
 app.set('view engine','ejs');

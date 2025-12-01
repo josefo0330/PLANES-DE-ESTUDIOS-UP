2520e725c2d4711c4e15c083ea764fcd7bf96459
josefo0330 <jose_6128@hotmail.com>
2025-11-30 21:43:18 -0500

    POOL

--- src/router/baseDataConect/conexion.js
+++ src/router/baseDataConect/conexion.js
@@ -1,6 +1,6 @@
  var mysql = require('mysql');
 
-   const pool = mysql.createPool({
+   const conexion = mysql.createPool({
         host: 'btiaphkz1zqolfvrqhg4-mysql.services.clever-cloud.com',
         database:"btiaphkz1zqolfvrqhg4",//'if0_36456925_cruv',
         user:'uxf3zc6yzkruyzpu',
